37f24e14d754398d4f75f18c489c915304569de6
catherine <abaker9906@gmail.com>
2023-01-14 14:39:58 +0000

    Added DI to Program.cs

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,18 @@
 ﻿using TestProject;
 using TestProject.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TestProject.Interfaces;
 
-var gameManager = new GameManager();
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IDisplayManager, DisplayManager>();
+        services.AddTransient<IGridGen, GridGen>();
+    })
+    .Build();
 
-var gridGenerator = new GridGen();
-var displayManager = new DisplayManager();
+var gameManager = new GameManager(host.Services.GetRequiredService<IGridGen>(),
+                                  host.Services.GetRequiredService<IDisplayManager>());
 
 gameManager.BeginGame();
